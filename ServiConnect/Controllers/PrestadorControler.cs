bcfecf0859fe2ec23984ec69bba0018e35c7044a
Fernando Cardoso Nascimento <fernando.cardosonasc@gmail.com>
2025-05-07 19:06:39 -0300

    sdsadf

---
+++ ServiConnect/Controllers/PrestadorControler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiConnect.Models;
+
+namespace ServiConnect.Controllers
+{
+    public class PrestadorControler : Controller
+    {
+        public readonly  AppDbContext _context;
+        public PrestadorControler(AppDbContext context)
+        {
+            _context = context; 
+        }
+
+        public async Task<IActionResult> Index ()
+        {
+            var dados = await _context.User.ToListAsync();     
+
+            return View (dados);
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();  
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+    }
+}
