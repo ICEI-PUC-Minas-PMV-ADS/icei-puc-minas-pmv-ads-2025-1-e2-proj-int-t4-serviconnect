174ef4f5c5ba8477934c73dde3acae9a9a48515e
Fernando Cardoso Nascimento <155500287+FernandoCardoso06@users.noreply.github.com>
2025-05-25 22:56:19 -0300

    Atualização Telas login excluidas

--- ServiConnect/obj/Debug/net8.0/ServiConnect.AssemblyInfo.cs
+++ ServiConnect/obj/Debug/net8.0/ServiConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a14211a1ba2fbc668ad411f809020acf5981b46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8bce053992ae91d0e099f9b41f1906e68736c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
