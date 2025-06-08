0e9aecb33d7e14f7d3a21c4a643d183da1dd5b0b
viniciusmdl <vinicius.lima.1565236@sga.pucminas.br>
2025-06-08 03:53:10 -0300

    Ajuste no login de administradores + integração com banco de dados , login indo para index de prestadores

--- ServiConnect/Controllers/LoginAdminController.cs
+++ ServiConnect/Controllers/LoginAdminController.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServiConnect.Models;
+using System.Linq;
 
 namespace ServiConnect.Controllers
 {
     public class LoginAdminController : Controller
     {
-        
+        private readonly AppDbContext _context;
+
+        // Construtor com injeção de dependência
+        public LoginAdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /LoginAdmin
         public IActionResult Index()
         {
             return View();
         }
 
-        
+        // POST: /LoginAdmin/Logar
         [HttpPost]
         public IActionResult Logar(LoginAdmin admin)
         {
-            if (admin.Email == "admin@serviconnect.com" && admin.Password == "123456")
+            if (!ModelState.IsValid)
+            {
+                return View("Index", admin);
+            }
+
+            // Busca no banco de dados
+            var adminExistente = _context.LoginAdmins
+                .FirstOrDefault(a => a.Email == admin.Email && a.Password == admin.Password);
+
+            if (adminExistente != null)
             {
                 TempData["Mensagem"] = "Login de administrador realizado com sucesso!";
-                return RedirectToAction("Dashboard"); // criar depois
+                return RedirectToAction("Index", "PrestadoresControler"); // vai para página do Fe
             }
             else
             {
@@ -27,7 +45,7 @@ namespace ServiConnect.Controllers
             }
         }
 
-       
+        // Página após login, se quiser
         public IActionResult Dashboard()
         {
             return View();
